a040e072bcf97d0bfc1e8b6751e2573b1c0176d8
shubhamkandekar <shubhamjkandekar@gmail.com>
2024-09-07 00:09:14 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:8080"); // Replace with your server URL
+const socket = io("https://driverse.onrender.com"); // Replace with your server URL
 
 let peerConnection;
 
