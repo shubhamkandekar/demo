3bbd64948c6bd781312c19d02765f15b0675f6d9
shubhamkandekar <shubhamjkandekar@gmail.com>
2024-09-06 18:07:04 +0530

    cutiya pa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import  { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:8080'); // Replace <YOUR_LOCAL_IP> with your IP address
+const socket = io('https://driverse.onrender.com'); // Replace <YOUR_LOCAL_IP> with your IP address
 
 function App() {
   const [myUserId, setMyUserId] = useState('');
